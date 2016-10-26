e96c50e1c5969966e9d4997c69981b57281e1456
SandipBanodha <sandip.banodha@bcssarl.com>
2016-10-26 11:32:40 +0530

    Add Action method in Sample controller

--- SampleGitHub/Controllers/SampleController.cs
+++ SampleGitHub/Controllers/SampleController.cs
@@ -13,5 +13,9 @@ namespace SampleGitHub.Controllers
         {
             return View();
         }
+        public ActionResult SampleList()
+        {
+            return View();
+        }
     }
 }
